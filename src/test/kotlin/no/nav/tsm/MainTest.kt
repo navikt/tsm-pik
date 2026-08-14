434400acf843bbf576e57d0fdf4a2605743f9d48
MikAoJk <joakimkartveit@gmail.com>
2026-08-14 14:28:04 +0200

    chore: fix packed structure

---
+++ src/test/kotlin/no/nav/tsm/MainTest.kt
@@ -0,0 +1,11 @@
+package no.nav.tsm
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+    @Test
+    fun `simple test`() {
+        assertEquals(expected = true, actual = true)
+    }
+}
