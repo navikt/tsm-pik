c68b320b9b8e1129c874a32cf20fbb469ba077fe
MikAoJk <joakimkartveit@gmail.com>
2026-08-17 14:30:50 +0200

    chore: added unit test for Environment vals

---
+++ src/test/kotlin/no/nav/tsm/core/EnvironmentTest.kt
@@ -0,0 +1,28 @@
+package no.nav.tsm.core
+
+import com.typesafe.config.ConfigFactory
+import io.ktor.server.config.HoconApplicationConfig
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class EnvironmentTest {
+    @Test
+    fun `production environment should be properly configured`() {
+        val applicationConfig =
+            HoconApplicationConfig(
+                ConfigFactory.parseMap(
+                    mapOf(
+                        "NAIS_POD_NAME" to "tsm-pik-prod-123",
+                        "NAIS_CLUSTER_NAME" to "prod-gcp",
+                    )
+                )
+                    .withFallback(ConfigFactory.parseResources("application.conf"))
+                    .resolve()
+            )
+
+        val environment = initializeEnvironment(applicationConfig)
+
+        assertEquals("tsm-pik-prod-123", environment.runtime.name)
+
+    }
+}
