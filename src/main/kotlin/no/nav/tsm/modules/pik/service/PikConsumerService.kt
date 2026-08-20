929fbeb49f35797e5dabd7469f2f063a0bff4ef0
MikAoJk <joakimkartveit@gmail.com>
2026-08-20 11:58:28 +0200

    chore: drop that import

--- src/main/kotlin/no/nav/tsm/modules/pik/service/PikConsumerService.kt
+++ src/main/kotlin/no/nav/tsm/modules/pik/service/PikConsumerService.kt
@@ -4,7 +4,6 @@ import no.nav.tsm.ktor.logger
 import no.nav.tsm.modules.etterlevelse.mapper.tilJuridiskVurderingKafkaMessage
 import no.nav.tsm.modules.etterlevelse.service.EtterlevelseProducerService
 import no.nav.tsm.modules.pik.model.JuridiskHenvisningRecord
-import kotlin.math.log
 
 class PikConsumerService(val etterlevelseProducerService: EtterlevelseProducerService) {
     private val logger = logger()
